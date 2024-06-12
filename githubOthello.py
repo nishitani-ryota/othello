f922469654f7cb46dd68157e0c8e7ae65297c048
nishitani-ryota <ryota_kuriki@yahoo.co.jp>
2024-06-12 17:19:45 +0900

    Add githubOthello.py

--- githubOthello.py
+++ githubOthello.py
@@ -3,44 +3,7 @@ pygame.init()
 
 #関数------------------------------------------------------------------------------------------------------
 #石を置ける場所の取得
-def full_potitions():
-    full_list = []
-    for row in range(square_num):
-        for col in range(4,12):
-            #石を置いていないマスのみチェック
-            if boards[row][col] == 0:
-                
-                for vx,vy in vec_table:
-                    x = vx + col
-                    y = vy +row
-                    #マスの範囲内、かつプレイやーの石と異なる意思がある場合、その方向は引き続きチェック
-                    if 4 <= x < 12 and 0 <= y < 8 and boards[y][x] == -player:
-                            while True:
-                                x += vx
-                                y += vy
-                                #プレイヤーの石と異なる石がある場合、その方向はひきつづきチェック
-                                if 4 <= x < 12 and 0 <= y < 8 and boards[y][x] == -player:
-                                    continue
-                                #プレイヤーの石と同色の石がある場合、石を置けるためのインデックスを保存
-                                elif 4 <= x < 12 and 0 <= y < 8 and boards[y][x] == player:
-                                    full_list.append((col,row))
-                                    break
-                                else:
-                                    break
-    return full_list
-#石をひっくり返す
-def full_pieces(col,row):
-    for vx,vy in vec_table:
-        flip_full_list = []
-        x = vx + col
-        y = vy + row
-        while 4 <= x < 12 and 0 <= y < 8 and boards[y][x] == -player:
-            flip_full_list.append((x,y))
-            x += vx
-            y += vy
-            if 4 <= x < 12 and 0 <= y < 8 and boards[y][x] == player:
-                for flip_x, flip_y in flip_full_list:
-                    boards[flip_y][flip_x] = player
+
 #グリッド線
 def draw_grid():
     if screen:
@@ -98,6 +61,7 @@ def flip_pieces(col,row):
                         board[flip_y][flip_x] = player
 #--------------------------------------------------------------------------------------------------------------------
 #ウィンドウ作成
+width = 100
 screen_width = 800
 screen_height = 800 
 screen = pygame.display.set_mode((screen_width,screen_height))
@@ -125,15 +89,6 @@ board = [
         [0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0]]
-boards = [
-    [2,2,2,2,0,0,0,0,0,0,0,0,2,2,2,2],
-    [2,2,2,2,0,0,0,0,0,0,0,0,2,2,2,2],
-    [2,2,2,2,0,0,0,0,0,0,0,0,2,2,2,2],
-    [2,2,2,2,0,0,0,1,-1,0,0,0,2,2,2,2],
-    [2,2,2,2,0,0,0,-1,1,0,0,0,2,2,2,2],
-    [2,2,2,2,0,0,0,0,0,0,0,0,2,2,2,2],
-    [2,2,2,2,0,0,0,0,0,0,0,0,2,2,2,2],
-    [2,2,2,2,0,0,0,0,0,0,0,0,2,2,2,2]]
 #プレイヤー
 player = 1
 vec_table = [
@@ -236,7 +191,8 @@ while run:
             #フルスクリーン
             if event.key == pygame.K_f:
                 full = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-                fullscreen = True   
+                fullscreen = True
+                run = False
             if event.key == pygame.K_ESCAPE:
                 run =False
                 fullscreen = False
@@ -255,15 +211,6 @@ while run:
                             board[y][x] = player
                             player *= -1
                             pass_num = 0
-                    # if fullscreen:
-                    #     x = mx // 100
-                    #     y = my // 100
-                    #     if board[y][x] == 0 and (x,y) in valid_position_list:
-                    #         #石をひっくり返す
-                    #         flip_pieces(x,y)
-                    #         board[y][x] = player
-                    #         player *= -1
-                    #         pass_num = 0
                 else:
                     board = [
                         [0,0,0,0,0,0,0,0],
@@ -276,54 +223,170 @@ while run:
                         [0,0,0,0,0,0,0,0]]
                     player = 1
                     game_over = False
-                    pass_num = 0
-    if fullscreen:
-    #背景の塗りつぶし
-    #full.fill(GREEN)
-        #colが1だったら黒石を置いて-1だったら白石を置く設定
-        for row_index,row in enumerate(boards):
-            for col_index,col in enumerate(row):
-                if col == 1:
-                    pygame.draw.circle(full,BLACK,(col_index * square_size + 50, row_index * square_size + 100),45)
-                elif col == -1:
-                    pygame.draw.circle(full,WHITE,(col_index * square_size + 50, row_index * square_size + 100),45) 
-        for i in range(square_num+1):
-            for line in range(4):
-                pygame.draw.line(full,GREEN,(0,line*square_size),(800,line*square_size),3)
-                pygame.draw.line(full,GREEN,(line*square_size,0),(line*square_size,800),3)
-            for line in range(4,9):
-                pygame.draw.line(full,GREEN,(0,line*square_size),(800,line*square_size),3)
-                pygame.draw.line(full,GREEN,(line*square_size,0),(line*square_size,50),3)
-                pygame.draw.line(full,BLACK,(400,i*square_size+50),(1200,i*square_size+50),3)
-                pygame.draw.line(full,BLACK,((i+4)*square_size,50),((i+4)*square_size,850),3)
-        #石を置ける場所の取得
-        full_list = full_potitions()
-        #石を置ける場所を黄色の円で表示
-        for x,y in full_list:
-            pygame.draw.circle(full,YELLOW,(x * square_size + 50, y * square_size + 100),45,3)
-        #イベントの取得     
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                fullscreen = False
-            #マウスクリック
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if game_over == False:
-                    mx,my = pygame.mouse.get_pos()
-                    x = mx // 100
-                    y = my // 100
-                    if boards[y][x] == 0 and (x,y) in full_list:
-                        #石をひっくり返す
-                        full_pieces(x,y)
-                        boards[y][x] = player
-                        player *= -1
-                        pass_num = 0
-
-
-                #横線 pygame.draw.line(screen,GREEN,(始点),(終点),3)   
+                    pass_num = 0 
     #更新
     pygame.display.update()
     clock.tick(FPS)
 ##################################################################################################
 
+screen_width = 800
+full = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+square_num = 8
+square_size = screen_width // square_num
+#fps設定
+FPS = 60
+clock = pygame.time.Clock()
+#盤面(黒:1,白:-1)
+
+player = 1
+vec_table = [
+    (-1,-1),    #左上
+    (0,-1),     #上
+    (1,-1),     #右上
+    (-1,0),     #左
+    (1,0),      #右
+    (-1,1),     #左下
+    (0,1),      #下
+    (1,1) ]     #右下
+game_over = False
+pass_num = 0
+board = [
+    [2,2,2,2,0,0,0,0,0,0,0,0,2,2,2,2],
+    [2,2,2,2,0,0,0,0,0,0,0,0,2,2,2,2],
+    [2,2,2,2,0,0,0,0,0,0,0,0,2,2,2,2],
+    [2,2,2,2,0,0,0,1,-1,0,0,0,2,2,2,2],
+    [2,2,2,2,0,0,0,-1,1,0,0,0,2,2,2,2],
+    [2,2,2,2,0,0,0,0,0,0,0,0,2,2,2,2],
+    [2,2,2,2,0,0,0,0,0,0,0,0,2,2,2,2],
+    [2,2,2,2,0,0,0,0,0,0,0,0,2,2,2,2]]
+#色の設定
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,128,0)
+BLUE = (0,0,255)
+YELLOW = (255,255,0)
+#表示文の設定
+font = pygame.font.SysFont(None, 100, bold=False, italic=False)
+
+#関数============================================
+#石を置ける場所の取得
+def full_potitions():
+    full_list = []
+    for row in range(square_num):
+        for col in range(4,12):
+            #石を置いていないマスのみチェック
+            if board[row][col] == 0:
+                
+                for vx,vy in vec_table:
+                    x = vx + col
+                    y = vy +row
+                    #マスの範囲内、かつプレイやーの石と異なる意思がある場合、その方向は引き続きチェック
+                    if 4 <= x < 12 and 0 <= y < 8 and board[y][x] == -player:
+                            while True:
+                                x += vx
+                                y += vy
+                                #プレイヤーの石と異なる石がある場合、その方向はひきつづきチェック
+                                if 4 <= x < 12 and 0 <= y < 8 and board[y][x] == -player:
+                                    continue
+                                #プレイヤーの石と同色の石がある場合、石を置けるためのインデックスを保存
+                                elif 4 <= x < 12 and 0 <= y < 8 and board[y][x] == player:
+                                    full_list.append((col,row))
+                                    break
+                                else:
+                                    break
+    return full_list
+#石をひっくり返す
+def full_pieces(col,row):
+    for vx,vy in vec_table:
+        flip_list = []
+        x = vx + col
+        y = vy + row
+        while 4 <= x < 12 and 0 <= y < 8 and board[y][x] == -player:
+            flip_list.append((x,y))
+            x += vx
+            y += vy
+            if 4 <= x < 12 and 0 <= y < 8 and board[y][x] == player:
+                for flip_x, flip_y in flip_list:
+                    board[flip_y][flip_x] = player
+#####################################################
+
+    #メイン#####################################################################################################################################################
+
+while fullscreen:
+    #背景の塗りつぶし
+    full.fill(GREEN)
+    #石を置ける場所の取得
+    full_list = full_potitions()
+    #イベントの取得     
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        #マウスクリック
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if game_over == False:
+                mx,my = pygame.mouse.get_pos()
+                x = mx // 100
+                if 50 <= my < 100: #0
+                    y = my // 100 
+                if 100 <= my < 150: #0
+                    y = my // 100 -1
+                if 150 <= my < 200: #1
+                    y = my // 100 
+                if 200 <= my < 250: #1
+                    y = my // 100 -1
+                if 250 <= my < 300: #2
+                    y = my // 100 
+                if 300 <= my < 350: #2
+                    y = my // 100 -1
+                if 350 <= my < 400:#3
+                    y = my // 100
+                if 400 <= my < 450: #3
+                    y = my // 100 -1
+                if 450 <= my < 500: #4
+                    y = my // 100
+                if 500 <= my < 550: #4
+                    y = my // 100 -1 
+                if 550 <= my < 600: #5
+                    y = my // 100
+                if 600 <= my < 650: #5
+                    y = my // 100 - 1
+                if 650 <= my < 700: #6
+                    y = my // 100
+                if 700 <= my < 750: #6
+                    y = my // 100 - 1
+                if 750 <= my <= 800: #7
+                    y = my // 100
+                if 800 <= my <= 850: #7
+                    y = my // 100 - 1
+                if board[y][x] == 0 and (x,y) in full_list:
+                    #石をひっくり返す
+                    full_pieces(x,y)
+                    board[y][x] = player
+                    player *= -1
+                    pass_num = 0
+    #colが1だったら黒石を置いて-1だったら白石を置く設定
+    for row_index,row in enumerate(board):
+        for col_index,col in enumerate(row):
+            if col == 1:
+                pygame.draw.circle(full,BLACK,(col_index * square_size + 50, row_index * square_size + 100),45)
+            elif col == -1:
+                pygame.draw.circle(full,WHITE,(col_index * square_size + 50, row_index * square_size + 100),45)
+    for i in range(square_num+1):
+        for line in range(4):
+            pygame.draw.line(full,GREEN,(0,line*square_size),(800,line*square_size),3)
+            pygame.draw.line(full,GREEN,(line*square_size,0),(line*square_size,800),3)
+        for line in range(4,9):
+            pygame.draw.line(full,GREEN,(0,line*square_size),(800,line*square_size),3)
+            pygame.draw.line(full,GREEN,(line*square_size,0),(line*square_size,50),3)
+            pygame.draw.line(full,BLACK,(400,i*square_size+50),(1200,i*square_size+50),3)
+            pygame.draw.line(full,BLACK,((i+4)*square_size,50),((i+4)*square_size,850),3)
+            #石を置ける場所の取得
+    full_list = full_potitions()
+    #石を置ける場所を黄色の円で表示
+    for x,y in full_list:
+        pygame.draw.circle(full,YELLOW,(x * square_size + 50, y * square_size + 100),45,3)
+    #更新
+    pygame.display.update()
+    clock.tick(FPS)
 pygame.quit()
